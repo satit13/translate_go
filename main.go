5a7ae3d06c0d01a0377d79209a5783ef6505c394
satit13 <satit@nopadol.com>
2018-01-30 16:43:52 +0700

    refactor to function

--- main.go
+++ main.go
@@ -14,27 +14,30 @@ import (
 
 func main() {
 	ctx := context.Background()
-
 	// Creates a client.
 	client, err := translate.NewClient(ctx, option.WithCredentialsFile("./secret/service_account.json"))
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
 	}
 
+	text := "my name is satit , i cannot to jumping but i am handsome "
+	tag := "th"
+	fmt.Printf("Text: %v\n", text)
+	fmt.Printf("Translation: %v\n", makeTranslate(ctx,client,text,tag))
+
+}
+func makeTranslate(ctx context.Context,client *translate.Client,text string,tag string) string{
 	// Sets the text to translate.
-	text := "Hello, world!"
 	// Sets the target language.
-	target, err := language.Parse("zh-CN")
+	target, err := language.Parse(tag)
 	if err != nil {
 		log.Fatalf("Failed to parse target language: %v", err)
 	}
 
-	// Translates the text into Russian.
+	// Translates the text into tag request.
 	translations, err := client.Translate(ctx, []string{text}, target, nil)
 	if err != nil {
 		log.Fatalf("Failed to translate text: %v", err)
 	}
-
-	fmt.Printf("Text: %v\n", text)
-	fmt.Printf("Translation: %v\n", translations[0].Text)
+	return translations[0].Text
 }
